7d1e44d37ba6b51debcf34f5eea3fa17fdd9aa4b
geetankr <g.raipuria@robotcaresystems.com>
2017-05-20 14:47:58 +0200

    Add m file to read images

---
+++ read_imgs.m
@@ -0,0 +1,24 @@
+% function  [images] = read_imgs(image_folder, extn)
+% Read multiple images from a directory with specified extention
+%
+% INPUT:
+%  - image_folder = folder in which the images are stored
+%  - extn = entention of the image files eg
+%
+% OUTPUT
+% - Structure with image data
+function  [images] = read_imgs(image_folder,extn)
+
+extn = strcat('*',extn);
+
+% read all images from the folder
+filenames = dir(fullfile(image_folder, extn));
+total_images = numel(filenames);
+
+images = struct;
+
+for n = 1:total_images
+    full_name= fullfile(image_folder, filenames(n).name);     
+    images(n).data = imread(full_name);            
+
+end
