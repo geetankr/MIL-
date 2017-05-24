049e40dacdc94792f89852abd487e3d37af1d626
geetankr <geetankr@gmail.com>
2017-05-24 23:57:55 +0200

    finally

--- MIL_run.m
+++ MIL_run.m
@@ -1,2 +1,12 @@
 % Code by Geetank raipuria (geetankr@gmail.com)
 
+clc
+clear all
+close all
+
+addpath('/home/geetank/Desktop/ML/Assign/additionalcode')
+
+imgs = read_imgs('/home/geetank/Desktop/ML/Assign/sival_apple_banana/apple', '.jpg');
+
+width = 50;
+lab = im_meanshift(imgs(1).data,width);
